4a7bff57e04e3601e04accbe55afed20c0c7d28d
umer 134 <twincki69@gmail.com>
2026-07-07 20:02:02 +0300

    almost ready to ci\cd

--- client/src/shared/api/endpoints.ts
+++ client/src/shared/api/endpoints.ts
@@ -2,26 +2,26 @@ import { env } from '@/config/env.config';
 
 export const API_ENDPOINTS = {
   AUTH: {
-    LOGIN: `${env.apiUrl}/api/login`,
-    REGISTER: `${env.apiUrl}/api/registration`,
-    LOGOUT: `${env.apiUrl}/api/logout`,
-    REFRESH: `${env.apiUrl}/api/refresh`,
-    ACTIVATE: `${env.apiUrl}/api/activate`,
+    LOGIN: `${env.apiUrl}/login`,
+    REGISTER: `${env.apiUrl}/registration`,
+    LOGOUT: `${env.apiUrl}/logout`,
+    REFRESH: `${env.apiUrl}/refresh`,
+    ACTIVATE: `${env.apiUrl}/activate`,
   },
   ME: {
-    GET: `${env.apiUrl}/api/me`,
-    UPDATE: `${env.apiUrl}/api/me`,
+    GET: `${env.apiUrl}/me`,
+    UPDATE: `${env.apiUrl}/me`,
   },
   USERS: {
     GET_ALL: `${env.apiUrl}/users`,
     GET_BY_ID: (id: string) => `${env.apiUrl}/users/${id}`,
     GET_BY_USERNAME: (username: string) =>
-      `${env.apiUrl}/api/users/search?username=${username}`,
+      `${env.apiUrl}/users/search?username=${username}`,
     UPDATE: (id: string) => `${env.apiUrl}/users/${id}`,
     DELETE: (id: string) => `${env.apiUrl}/users/${id}`,
   },
   CHATS: {
-    GET_ALL: `${env.apiUrl}/api/chats`,
+    GET_ALL: `${env.apiUrl}/chats`,
     GET_BY_ID: (id: string) => `${env.apiUrl}/chats/${id}`,
     CREATE: `${env.apiUrl}/chats`,
     UPDATE: (id: string) => `${env.apiUrl}/chats/${id}`,
@@ -29,11 +29,11 @@ export const API_ENDPOINTS = {
   },
   MESSAGES: {
     GET: (chatId: string) => `${env.apiUrl}/messages/${chatId}`,
-    GET2: (chatId: string) => `${env.apiUrl}/api/chats/${chatId}/messages`,
+    GET2: (chatId: string) => `${env.apiUrl}/chats/${chatId}/messages`,
     GET_BY_ID: (id: string) => `${env.apiUrl}/messages/${id}`,
-    CREATE: `${env.apiUrl}/api/messages`,
-    READ: (chatId: string) => `${env.apiUrl}/api/messages/read/${chatId}`,
-    UPDATE: `${env.apiUrl}/api/messages`,
-    DELETE: (id: string) => `${env.apiUrl}/api/messages/${id}`,
+    CREATE: `${env.apiUrl}/messages`,
+    READ: (chatId: string) => `${env.apiUrl}/messages/read/${chatId}`,
+    UPDATE: `${env.apiUrl}/messages`,
+    DELETE: (id: string) => `${env.apiUrl}/messages/${id}`,
   },
 } as const;
